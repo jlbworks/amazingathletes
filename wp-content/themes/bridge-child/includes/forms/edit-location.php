c4d1cdfe56284584831d57c822cf110c140d0588
isvaljek <ivan.svaljek@gmail.com>
2016-04-18 09:09:51 +0200

    forms padding and margins

--- wp-content/themes/bridge-child/includes/forms/edit-location.php
+++ wp-content/themes/bridge-child/includes/forms/edit-location.php
@@ -1,84 +1,85 @@
-<?php 
+<?php
 global $wpdb;
-$loc_id=0;
-$city_state = array('','');	
+$loc_id = 0;
+$city_state = array('', '');
 
-if(!empty($_GET['loc_id'])){
-	$loc_id=$_GET['loc_id'];
+if (!empty($_GET['loc_id'])) {
+	$loc_id = $_GET['loc_id'];
 	$pmeta = get_post_meta($loc_id); //, 'city__state',true);
-	if(!empty($pmeta['city__state'][0])) $city_state = explode('|', $pmeta['city__state'][0]);	
+	if (!empty($pmeta['city__state'][0])) {
+		$city_state = explode('|', $pmeta['city__state'][0]);
+	}
+
 	$location = get_post($loc_id);
 }
 
-if( (!empty($location) && $location->post_author == $user->ID) || isset($_GET['add']) ) {
+if ((!empty($location) && $location->post_author == $user->ID) || isset($_GET['add'])) {
 
-?>	
+	?>
 <form id="frm_edit_location" action="<?php echo admin_url('admin-ajax.php') ?>" method="POST">
 	<label>Location Type *</label>
-	<?php 
-	$field_key = "field_570b6ef56c895";
+	<?php
+$field_key = "field_570b6ef56c895";
 	$field = get_field_object($field_key);
 	$location_type = get_post_meta($loc_id, 'location_type', true);
 
-	if( $field )
-	{
+	if ($field) {
 		echo '<select name="location_type" required>';
-		foreach( $field['choices'] as $k => $v )
-		{
-			echo '<option '. ($k == $location_type ? 'selected' : '').' value="' . $k . '">' . $v . '</option>';
+		foreach ($field['choices'] as $k => $v) {
+			echo '<option ' . ($k == $location_type ? 'selected' : '') . ' value="' . $k . '">' . $v . '</option>';
 		}
 		echo '</select>';
 	}?><br/>
 
 	<label>Location Name *</label>
-	<input type="text" name="location_name" maxlength="128" style="width:98%;" value="<?php echo get_post_meta($loc_id,'location_name',true);?>" required><br/>
+	<input type="text" name="location_name" maxlength="128" style="width:98%;" value="<?php echo get_post_meta($loc_id, 'location_name', true); ?>" required><br/>
 
 	<label>Address *</label>
-	<input type="text" name="address" maxlength="128" style="width:98%;" value="<?php echo get_post_meta($loc_id,'address',true);?>" required><br/>
+	<input type="text" name="address" maxlength="128" style="width:98%;" value="<?php echo get_post_meta($loc_id, 'address', true); ?>" required><br/>
 
 	<label>State *</label>
-	<select name="state"  placeholder="Select a state..." class="am2_cc_state" required>
-		<option value=""></option>		
+	<select name="state"  placeholder="Select a state..." class="am2_cc_state" required style="width:98%;">
+		<option value=""></option>
 		<option value="">Select a state...</option>
-		<?php 
-		$states_db = $wpdb->get_results("SELECT DISTINCT * FROM states ORDER BY state ASC"); 	
-		$states = array();
-		if ($states_db) {
-			foreach ($states_db AS $state) {?>
-			<option <?php echo ($state->state_code == $city_state[0] ? 'selected' : '' );?> value="<?php echo $state->state_code;?>"><?php echo $state->state;?></option>						
+		<?php
+$states_db = $wpdb->get_results("SELECT DISTINCT * FROM states ORDER BY state ASC");
+	$states = array();
+	if ($states_db) {
+		foreach ($states_db AS $state) {?>
+			<option <?php echo ($state->state_code == $city_state[0] ? 'selected' : ''); ?> value="<?php echo $state->state_code; ?>"><?php echo $state->state; ?></option>
 			<?php }
-		}
-		?>			
+	}
+	?>
 	</select><br/>
 
 	<label>City *</label>
-	<input type="text" name="city" required maxlength="128" style="width:98%;" value="<?php echo $city_state[1];?>" class="am2_cc_city" required><br/>
+	<input type="text" name="city" required maxlength="128" style="width:98%;" value="<?php echo $city_state[1]; ?>" class="am2_cc_city" required><br/>
 
 	<input type="hidden" name="city__state" class="cc_city_state" required/><br/>
 	<input type="hidden" name="latlng" class="latlng" />
 
 	<label>Zip *</label>
-	<input type="text" name="zip" maxlength="10" size="10" value="<?php echo get_post_meta($loc_id,'zip',true);?>" required><br/>
+	<input type="text" name="zip" maxlength="10" size="10" value="<?php echo get_post_meta($loc_id, 'zip', true); ?>" required><br/>
 
 
 	<label>Telephone *</label>
-	<input type="text" name="telephone" maxlength="20" size="20" value="<?php echo get_post_meta($loc_id,'telephone',true);?>" required><br/>
+	<input type="text" name="telephone" maxlength="20" size="20" value="<?php echo get_post_meta($loc_id, 'telephone', true); ?>" required><br/>
 
 
 	<label>Fax</label>
-	<input type="text" name="fax" maxlength="20" size="20" value="<?php echo get_post_meta($loc_id,'fax',true);?>"><br/>
+	<input type="text" name="fax" maxlength="20" size="20" value="<?php echo get_post_meta($loc_id, 'fax', true); ?>"><br/>
 
 
 	<label>Email</label>
-	<input type="text" name="email" maxlength="255" style="width:98%;" value="<?php echo get_post_meta($loc_id,'email',true);?>"><br/>
+	<input type="text" name="email" maxlength="255" style="width:98%;" value="<?php echo get_post_meta($loc_id, 'email', true); ?>"><br/>
 
 
 	<label>Website</label>
-	<input type="text" name="website" maxlength="255" style="width:98%;" value="<?php echo get_post_meta($loc_id,'website',true);?>"><br/>
+	<input type="text" name="website" maxlength="255" style="width:98%;" value="<?php echo get_post_meta($loc_id, 'website', true); ?>"><br/>
 
 
 	<label>Director *</label>
-	<input type="text" name="director" maxlength="128" style="width:98%;" value="<?php echo get_post_meta($loc_id,'director',true);?>" required><br/>
+	<input type="text" name="director" maxlength="128" style="width:98%;" value="<?php echo get_post_meta($loc_id, 'director', true); ?>" required><br/>
 
 	<?php /*<input type="hidden" name="user_id" value="<?php echo $user->ID; ?>"/>*/?>
 	<input type="hidden" name="loc_id" value="<?php echo $loc_id; ?>"/>
@@ -89,6 +90,6 @@ if( (!empty($location) && $location->post_author == $user->ID) || isset($_GET['a
 <div id="map"></div>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA4rcwbMAQu0UW62G-dQpZTlBcJXj-rMXE"></script>
 <script>
-var permalink = '<?php echo get_permalink( );?>';
+var permalink = '<?php echo get_permalink(); ?>';
 </script>
-<?php } ?>
+<?php }?>
--- wp-content/themes/bridge-child/includes/forms/my-account-franchisee.php
+++ wp-content/themes/bridge-child/includes/forms/my-account-franchisee.php
@@ -15,86 +15,86 @@ $city_state = explode('|', $umeta['city__state'][0]);
 		<input type="text" name="franchise_address" required maxlength="128" style="width:98%;" value="<?=$umeta['mailing_address'][0];?>"><br/>
 
 		<label>State *</label>
-		<select name="franchise_state" required placeholder="Select a state..." class="am2_cc_state">
-			<option value=""></option>		
+		<select name="franchise_state" required placeholder="Select a state..." class="am2_cc_state" style="width:98%;">
+			<option value=""></option>
 			<option value="">Select a state...</option>
-			<?php 
-			$states_db = $wpdb->get_results("SELECT DISTINCT * FROM states ORDER BY state ASC");
-			$states = array();
-			if ($states_db) {
-				foreach ($states_db AS $state) {?>
-				<option <?php echo ($state->state_code == $city_state[0] ? 'selected' : '' );?> value="<?php echo $state->state_code;?>"><?php echo $state->state;?></option>						
+			<?php
+$states_db = $wpdb->get_results("SELECT DISTINCT * FROM states ORDER BY state ASC");
+$states = array();
+if ($states_db) {
+	foreach ($states_db AS $state) {?>
+				<option <?php echo ($state->state_code == $city_state[0] ? 'selected' : ''); ?> value="<?php echo $state->state_code; ?>"><?php echo $state->state; ?></option>
 				<?php }
-			}
-			?>			
+}
+?>
 		</select>
 
 		</select><br/>
-		
+
 		<label>City *</label>
-		<input type="text" name="franchise_city" required maxlength="128" style="width:98%;" value="<?php echo $city_state[1];?>" class="am2_cc_city"><br/>
+		<input type="text" name="franchise_city" required maxlength="128" style="width:98%;" value="<?php echo $city_state[1]; ?>" class="am2_cc_city"><br/>
 
 		<input type="hidden" name="franchise_city_state" class="cc_city_state" />
 
 
 		<label>ZIP Code *</label>
-		<input type="text" name="franchise_zip" required maxlength="10" size="10" value="<?php echo $umeta['zip_code'][0];?>"><br/>
+		<input type="text" name="franchise_zip" required maxlength="10" size="10" value="<?php echo $umeta['zip_code'][0]; ?>"><br/>
 
 
 		<label>Telephone *</label>
-		<input type="text" name="franchise_telephone" required maxlength="20" size="20" value="<?php echo $umeta['telephone'][0];?>"><br/>
+		<input type="text" name="franchise_telephone" required maxlength="20" size="20" value="<?php echo $umeta['telephone'][0]; ?>"><br/>
 
 
 		<label>Fax</label>
-		<input type="text" name="franchise_fax" maxlength="20" size="20" value="<?php echo $umeta['fax'][0];?>"><br/>
+		<input type="text" name="franchise_fax" maxlength="20" size="20" value="<?php echo $umeta['fax'][0]; ?>"><br/>
 
 
 		<label>Email Address *</label>
-		<input type="text" name="franchise_email" required data-rule-email="true" maxlength="128" style="width:98%;" value="<?php echo $umeta['email_address'][0];?>"><br/>
+		<input type="text" name="franchise_email" required data-rule-email="true" maxlength="128" style="width:98%;" value="<?php echo $umeta['email_address'][0]; ?>"><br/>
 
 
 		<label>AA Email Address</label>
-		<input type="text" name="franchise_aaemail" data-rule-email="true" maxlength="128" style="width:98%;" value="<?php echo $umeta['aa_email_address'][0];?>"><br/>
+		<input type="text" name="franchise_aaemail" data-rule-email="true" maxlength="128" style="width:98%;" value="<?php echo $umeta['aa_email_address'][0]; ?>"><br/>
 
 
 		<label>Website Address</label>
-		<input type="text" name="franchise_website" maxlength="128" style="width:98%;" value="<?php echo $umeta['website_address'][0];?>"><br/>
+		<input type="text" name="franchise_website" maxlength="128" style="width:98%;" value="<?php echo $umeta['website_address'][0]; ?>"><br/>
 
 
 		<?php /*<label>Login Password</label>
-		<input type="text" name="franchise_password" maxlength="8" size="8;" value="kardio"><br/>*/ ?>
+<input type="text" name="franchise_password" maxlength="8" size="8;" value="kardio"><br/>*/?>
 
 
 		<label>Market Area</label>
-		<td><textarea name="franchise_market" rows="2" style="width:98%;"><?php echo $umeta['market_area'][0];?></textarea></td>
+		<td><textarea name="franchise_market" rows="2" style="width:98%;"><?php echo $umeta['market_area'][0]; ?></textarea></td>
 
 
 		<label> Facebook Page</label>
-		<input type="text" name="franchise_facebook" maxlength="255" style="width:98%;" value="<?php echo $umeta['facebook_page'][0];?>"><br/>
+		<input type="text" name="franchise_facebook" maxlength="255" style="width:98%;" value="<?php echo $umeta['facebook_page'][0]; ?>"><br/>
 
 
 		<label> YouTube Page</label>
-		<input type="text" name="franchise_youtube" maxlength="255" style="width:98%;" value="<?php echo $umeta['youtube_page'][0];?>"><br/>
+		<input type="text" name="franchise_youtube" maxlength="255" style="width:98%;" value="<?php echo $umeta['youtube_page'][0]; ?>"><br/>
 
 
 		<label> Twitter Page</label>
-		<input type="text" name="franchise_twitter" maxlength="255" style="width:98%;" value="<?php echo $umeta['twitter_page'][0];?>"><br/>
+		<input type="text" name="franchise_twitter" maxlength="255" style="width:98%;" value="<?php echo $umeta['twitter_page'][0]; ?>"><br/>
 
 
 		<label> Pinterest Page</label>
-		<input type="text" name="franchise_pinterest" maxlength="255" style="width:98%;" value="<?php echo $umeta['pinterest_page'][0];?>"><br/>
+		<input type="text" name="franchise_pinterest" maxlength="255" style="width:98%;" value="<?php echo $umeta['pinterest_page'][0]; ?>"><br/>
 
 		<div class="franchisee_photo_wrap">
-			<?php 
-			$custom_image = get_field('franchisee_photo', 'user_' . $user->ID );
-			if( $custom_image ){ $custom_image_url = wp_get_attachment_image_src($custom_image, 'thumbnail'); ?>							
-				<img src="<?php echo $custom_image_url[0];?>" width="175"/>
-				<br/>				
-				<a class='delete_button button small-button' id='btn_delete_franchisee_photo' data-attid="<?php echo $custom_image;?>" >Delete image</a>
+			<?php
+$custom_image = get_field('franchisee_photo', 'user_' . $user->ID);
+if ($custom_image) {$custom_image_url = wp_get_attachment_image_src($custom_image, 'thumbnail');?>
+				<img src="<?php echo $custom_image_url[0]; ?>" width="175"/>
+				<br/>
+				<a class='delete_button button small-button' id='btn_delete_franchisee_photo' data-attid="<?php echo $custom_image; ?>" >Delete image</a>
 			<?php } else {?>
 				<div id="digital_image_upload"></div>
-			<?php } ?>
-			
+			<?php }?>
+
 		</div>
 
 		<?php /*<input type="hidden" name="user_id" value="<?php echo $user->ID; ?>"/> */?>
@@ -110,15 +110,15 @@ uploadOptions = {
 	template: "qq-simple-thumbnails-template",
 	thumbnails: {
 	    placeholders: {
-			waitingPath: "<?php bloginfo('template_directory'); ?>/img/waiting-generic.png",
-	        notAvailablePath: "<?php bloginfo('template_directory'); ?>/img/not_available-generic.png"
+			waitingPath: "<?php bloginfo('template_directory');?>/img/waiting-generic.png",
+	        notAvailablePath: "<?php bloginfo('template_directory');?>/img/not_available-generic.png"
 		}
 	},
 	request: {
-	    endpoint: '<?php echo admin_url( 'admin-ajax.php' );?>',
+	    endpoint: '<?php echo admin_url('admin-ajax.php'); ?>',
 	    params: {
 	        action: 'upload_franchise_photo',
-			user_id: '<?php echo $user->ID; ?>',			
+			user_id: '<?php echo $user->ID; ?>',
 	    }
 	},
 	validation: {
