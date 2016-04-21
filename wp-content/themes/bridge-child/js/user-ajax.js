bb2e7fa61b9c852603b72d7befeb5cd5a8876287
isvaljek <ivan.svaljek@gmail.com>
2016-04-21 11:48:44 +0200

    add coach fields validation

--- wp-content/themes/bridge-child/js/user-ajax.js
+++ wp-content/themes/bridge-child/js/user-ajax.js
@@ -253,11 +253,23 @@
 
          $('.btn_add_coach').on('click', function(e){
             e.preventDefault();
+
+            if($.trim($('#first_name').val()) == '' || $.trim($('#first_name').val()) == '' || $.trim($('#coach_email').val()) == ''){
+                alert("Please fill all the fields");
+                return;
+            }
+
+            if(!isValidEmailAddress($.trim($('#coach_email').val())) ){
+                alert("Please enter a valid email address");
+                return;   
+            }
+
             $.post(ajax_login_object.ajaxurl, {action: 'am2_add_coach', first_name: $('#first_name').val(), last_name: $('#last_name').val(), coach_email: $('#coach_email').val() }, function(resp){
                 if(resp.status == 'success') {
+
                     alert('Successfully added coach');
-                    var select_coaches = $select_coaches[0].selectize;
-                    console.log(resp);
+
+                    var select_coaches = $select_coaches[0].selectize;                    
                     select_coaches.addOption({value: resp.user_id, text: $('#first_name').val() + ' ' + $('#last_name').val()});
                     select_coaches.addItem(resp.user_id);
                     select_coaches.refreshOptions();
@@ -268,6 +280,11 @@
          });
 
     });    
+
+    function isValidEmailAddress(emailAddress) {
+        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        return pattern.test(emailAddress);
+    };
     
     function initMap() {
       var myLatLng = $('.latlng').val().split(',');
