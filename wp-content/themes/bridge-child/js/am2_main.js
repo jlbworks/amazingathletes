a44ea8a0d53be6962ed38472878caf2174904eda
isvaljek <ivan.svaljek@gmail.com>
2016-05-14 07:40:22 +0200

    sorting the /locations cities dropdown

--- wp-content/themes/bridge-child/js/am2_main.js
+++ wp-content/themes/bridge-child/js/am2_main.js
@@ -364,7 +364,7 @@
 	                    $li.append(k);                    
 	                    
 	                    $ul.append($li);
-	                });                
+	                });      
 
 	                $state.append($ul);
 
@@ -392,6 +392,16 @@
 		            });
 
 	                $('.dynamic-locaion-content').html($state);
+
+                    var options = $('select.cities option');
+                    var arr = options.map(function(_, o) { return { t: $(o).text(), v: o.value }; }).get();
+                    arr.sort(function(o1, o2) { return o1.t > o2.t ? 1 : o1.t < o2.t ? -1 : 0; });
+                    options.each(function(i, o) {
+                      o.value = arr[i].v;
+                      $(o).text(arr[i].t);
+                    });  
+
+                    $('select.cities').html(options);
 
 	                $('html, body').animate({scrollTop: $('.dynamic-locaion-content').eq(0).offset().top},500); 	                
 
