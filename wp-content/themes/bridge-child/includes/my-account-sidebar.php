b5f4830722658f0e5143d1573566dd88102cbfe8
JLB <web@jlbworks.com>
2016-06-24 07:59:00 -0500

    Removed two sidebar links
    Testimonials and registration

--- wp-content/themes/bridge-child/includes/my-account-sidebar.php
+++ wp-content/themes/bridge-child/includes/my-account-sidebar.php
@@ -85,26 +85,6 @@
         </a>
     </div>
     <div class="side-nav">
-        <a href="<?php echo site_url();?>/registration/" class="sidebar-link">
-            <span>
-                <img src="<?php echo site_url();?>/wp-content/uploads/2016/03/my-account-volleyball-icon.png" width="30px" class="spt-icons" id="vball2">
-            </span>
-            <span class="sidebar-nav">
-                <h2>REGISTRATION</h2>
-            </span>
-        </a>
-    </div>
-    <div class="side-nav">
-        <a href="<?php echo site_url();?>/testimonials/" class="sidebar-link">
-            <span>
-                <img src="<?php echo site_url();?>/wp-content/uploads/2016/03/my-locations-soccerball-icon.png" width="30px" class="spt-icons" id="sball2">
-            </span>
-            <span class="sidebar-nav">
-                <h2>TESTIMONIALS</h2>
-            </span>
-        </a>
-    </div>
-    <div class="side-nav">
         <a href="<?php echo site_url();?>/my-account/my-staff/" class="sidebar-link">
             <span>
                 <img src="<?php echo site_url();?>/wp-content/uploads/2016/03/logout-baseball-icon.png" width="30px" class="spt-icons" id="bsball2">
