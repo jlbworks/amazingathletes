7a8f6209d39adbd55f45f831bfefcdcc5588203b
JLB <web@jlbworks.com>
2016-06-24 07:41:46 -0500

    Removed spacing
    removed &nbsp;-&nbsp;

--- wp-content/themes/bridge-child/includes/forms/list-locations.php
+++ wp-content/themes/bridge-child/includes/forms/list-locations.php
@@ -46,7 +46,7 @@ $locations = get_posts(
 			<li class="franchise">
 				<h3>
 					<?php echo get_the_title( $loc->ID );?>&nbsp;-&nbsp;<span class="franchise_address"><?php echo implode("-", array(get_post_meta($loc->ID, 'address',true)));?></span>&nbsp;-&nbsp;
-					<div class="edit-btn"><a href="<?php echo get_permalink() . '?loc_id=' . $loc->ID;?>">Edit</a></div>&nbsp;-&nbsp;
+					<div class="edit-btn"><a href="<?php echo get_permalink() . '?loc_id=' . $loc->ID;?>">Edit</a></div>
 					<form data-form="frm_delete_location" action="<?php echo admin_url('admin-ajax.php') ?>" method="POST" >
 						<input type="hidden" name="action" value="am2_delete_location">
 						<input type="hidden" name="loc_id" value="<?php echo $loc->ID; ?>"/>
