610373c79f11e524553887493b6371cb02e9a424
Tomislav Vlahović <tomislav@am2studio.hr>
2016-07-21 16:38:50 +0200

    my calendar occurences are working

--- wp-content/themes/bridge-child/includes/my-account-calendar.php
+++ wp-content/themes/bridge-child/includes/my-account-calendar.php
@@ -1,4 +1,72 @@
 <?php
+require('When/Valid.php');
+require('When/When.php');
+
+//var_dump(date('Y-m-d', strtotime("first saturday of this month")));
+/*$start    = new DateTime('2016-07-01');
+$end      = new DateTime('2016-07-31');
+$interval = DateInterval::createFromDateString('first saturday of this month');
+$period   = new DatePeriod($start, $interval, $end, DatePeriod::EXCLUDE_START_DATE);
+foreach($period as $time) {
+    echo $time->format("F jS") . "<br>\n";
+}*/
+
+//exit();
+
+function am2_get_occurrences($_class) {
+    $r = new When\When();
+
+    if (date('l') == $_class->day) {
+        $r->startDate(new DateTime(date('Y-m-d')));
+    } else {
+        $r->startDate(new DateTime(date('Y-m-d', strtotime("next {$_class->day}"))));
+    }
+
+    $r->count(365);
+
+    if ('Weekly' === $_class->schedule_type) {
+        $r->byday(substr($_class->day, 0, 2));
+        $r->freq('weekly');
+    }
+
+    if ('Monthly' == $_class->schedule_type) {
+        $r->startDate(new DateTime(date('Y-m-d', strtotime("{$_class->monthly_every} {$_class->day} of this month"))));
+        $r->byday(substr($_class->day, 0, 2));
+        $r->count(365);
+        $r->freq('monthly');
+        //$r->bymonthday(1);
+    }
+
+    if ('Yearly' == $_class->schedule_type) {
+        $this_year = date('Y');
+        $r->startDate(new DateTime(date("{$this_year}-m-d", strtotime("{$_class->date_every_year}"))));
+        //$r->bymonthday(date('d', strtotime("{$_class->day}")));
+        $r->count(10);
+        $r->freq('yearly');
+    }
+
+    $r->generateOccurrences();
+
+    return $r->occurrences;
+}
+
+function am2_format_event_for_calendar($_class, $data=array()) {
+    $title = "{$_class->program}";
+
+    if ($_class->date) {
+        $start = date('Y-m-d', strtotime("{$_class->date}"));
+    }
+
+    if (isset($data['start'])) {
+        $start = $data['start'];
+    }
+
+    return array(
+        'title' => $title,
+        'start' => $start,
+    );
+}
+
 $locations = get_posts(
     array(
         'post_type'         => 'location',
@@ -8,7 +76,7 @@ $locations = get_posts(
     )
 );
 
-$_locations = array();
+$_locations             = array();
 $locations_for_calendar = array();
 
 foreach ($locations as $l) {
@@ -39,25 +107,36 @@ $args = array(
 );
 
 $classes = get_posts($args);
+
 $classes_for_calendar = [];
 
 foreach ($classes as $c) {
+    //var_dump(get_post_meta($c->ID));
+
+    if (in_array($c->type, array('Parent-Pay', 'Session'))) {
+
+        $occurrences = am2_get_occurrences($c);
+
+        foreach ($occurrences as $o) {
+
+            $classes_for_calendar[] = am2_format_event_for_calendar($c, array('start' => $o->format('Y-m-d')));
+        }
+
+        continue;
+    }
+
     if (!$c->date) {
         continue;
     }
 
-    //$title = "{$locations_for_calendar[$c->location_id]['post_title']} - {$c->type}";
-    $title = "{$c->type}";
-    $classes_for_calendar[] = array(
-        'title' => $title,
-        'start' => date('Y-m-d', strtotime("{$c->date} {$c->time}")),
-    );
+    $classes_for_calendar[] = am2_format_event_for_calendar($c);
 }
 //exit();
 ?>
 <div class="kolona1">
     <label>Location:</label>
     <select id="filter_location" name="location_id">
+        <option></option>
     <?php foreach ($locations as $l): ?>
         <option value="<?php echo $l->ID; ?>"><?php echo $l->post_title; ?></option>
     <?php endforeach; ?>
@@ -65,6 +144,7 @@ foreach ($classes as $c) {
 
     <label>Month:</label>
     <select id="filter_location" name="location_id">
+        <option></option>
     <?php for ($m=1; $m<=12; $m++): $month_name = date('F', mktime(0,0,0,$m, 1, date('Y'))); ?>
         <option value="<?php echo $m; ?>"><?php echo $month_name; ?></option>
     <?php endfor; ?>
@@ -72,6 +152,7 @@ foreach ($classes as $c) {
 
     <label>Coach:</label>
     <select id="filter_coach" name="coach_id">
+        <option></option>
     <?php foreach ($coaches as $c): ?>
         <option value="<?php echo $c->ID; ?>"><?php echo "{$c->first_name} {$c->last_name}"; ?></option>
     <?php endforeach; ?>
