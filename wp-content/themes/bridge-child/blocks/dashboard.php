7f3cc0ccf04982585d4c809a54663b5ec6181ea7
Cw1X <cwixpro@gmail.com>
2016-08-15 13:01:28 +0200

    - Dashboard

--- wp-content/themes/bridge-child/blocks/dashboard.php
+++ wp-content/themes/bridge-child/blocks/dashboard.php
@@ -7,30 +7,41 @@ if( is_role('client') ){
 
 
 $args = array(
-  'post_type'   => 'bolnice',
-  'post_status' => 'publish',
-  'posts_per_page'=>-1
+  'role' => 'franchisee'
 );
-$ukupno_bolnica = count(get_posts($args));
+$total_franchises = count(get_users($args));
 
 
 $args = array(
-  'post_type'   => 'podaci',
+  'post_type'   => 'location',
   'post_status' => 'publish',
   'posts_per_page'=>-1
 );
-$ukupno_podataka = count(get_posts($args));
+if( is_role( 'franchisee' ) ) {
+    $args['author'] = get_current_user_id();
+}
+$total_locations = count(get_posts($args));
 
 
 $args = array(
-  'post_type'   => 'pacijenti',
-  'post_status' => 'publish',
-  'posts_per_page'=>-1
+ 'role' => 'coach'
 );
-$total_pacijenti = count(get_posts($args));
+if( is_role( 'franchisee' ) ) {
+    $args['meta_key'] = 'franchisee';
+    $args['meta_value'] = get_current_user_id();
+    $args['meta_compare'] = '=';
+}
+$total_coaches = count(get_users($args));
 
-
-$total_users = count(get_users());
+$args = array(
+    'post_type'   => 'customer',
+    'post_status' => 'publish',
+    'posts_per_page'=>-1
+);
+if( is_role( 'franchisee' ) ) {
+    $args['author'] = get_current_user_id();
+}
+$total_customers = count(get_posts($args));
 
 
 ?>
@@ -39,53 +50,51 @@ $total_users = count(get_users());
     <div class="container clearfix">
         <div class="col-12 break-big">
             <div class="card-wrapper">
-                <h3 class="card-header">Informacije</h3>
+                <h3 class="card-header">Information</h3>
                 <div class="card-inner">
                     <div class="card-table">
 
+                        <?php if( !is_role( 'franchisee' ) ): ?>
                         <div class="card-table-row">
-                            <span class="card-table-cell fixed250">Ukupno Bolnica</span>
+                            <span class="card-table-cell fixed250">Total Franchises</span>
                             <div class="card-table-cell">
                                 <form class="card-form no-inline-edit js-ajax-form">
                                 <fieldset>
-                                <?php echo $ukupno_bolnica; ?>
-                                <a class="text-muted text-uppercase" href="#bolnice">(view all)</a>
+                                <?php echo $total_franchises; ?>
+                                </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                        <div class="card-table-row">
+                            <span class="card-table-cell fixed250">Total Locations</span>
+                            <div class="card-table-cell">
+                                <form class="card-form no-inline-edit js-ajax-form">
+                                <fieldset>
+                                <?php echo $total_locations; ?>
                                 </fieldset>
                                 </form>
                             </div>
                         </div>
 
                         <div class="card-table-row">
-                            <span class="card-table-cell fixed250">Ukupno Doktora</span>
+                            <span class="card-table-cell fixed250">Total Coaches</span>
                             <div class="card-table-cell">
                                 <form class="card-form no-inline-edit js-ajax-form">
                                 <fieldset>
-                                <?php echo $total_users; ?>
-                                <a class="text-muted text-uppercase" href="#users-management">(view all)</a>
+                                <?php echo $total_coaches; ?>
                                 </fieldset>
                                 </form>
                             </div>
                         </div>
 
                         <div class="card-table-row">
-                            <span class="card-table-cell fixed250">Ukupno Pacijenata</span>
+                            <span class="card-table-cell fixed250">Total Customers</span>
                             <div class="card-table-cell">
                                 <form class="card-form no-inline-edit js-ajax-form">
                                 <fieldset>
-                                <?php echo $total_pacijenti; ?>
-                                <a class="text-muted text-uppercase" href="#pacijenti">(view all)</a>
-                                </fieldset>
-                                </form>
-                            </div>
-                        </div>
-
-                        <div class="card-table-row">
-                            <span class="card-table-cell fixed250">Ukupno Podataka</span>
-                            <div class="card-table-cell">
-                                <form class="card-form no-inline-edit js-ajax-form">
-                                <fieldset>
-                                <?php echo $ukupno_podataka; ?>
-                                <a class="text-muted text-uppercase" href="#podaci">(view all)</a>
+                                <?php echo $total_customers; ?>
+                                <a class="text-muted text-uppercase" href="#customers">(view all)</a>
                                 </fieldset>
                                 </form>
                             </div>
