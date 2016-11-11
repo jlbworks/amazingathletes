d63d2fcb7414bfac824f325526531386bdccada6
JLB <mitch@jlbworks.com>
2016-11-11 13:05:37 -0600

    did it again

--- wp-content/themes/bridge-child/erp-sidebar.php
+++ wp-content/themes/bridge-child/erp-sidebar.php
@@ -38,11 +38,11 @@
             <li>
                 <a href="#location-invoices"><i class="fa fa-users" aria-hidden="true"></i> <span>Location Invoices</span> </a>
             </li>
-            <?php } if( is_role('administrator') ){ ?>
+            <?php } if( is_role('administrator') ||  is_role('franchisee') ||  is_role('coach') ){ ?>
             <li>
                 <a href="#rss"><i class="fa fa-archive" aria-hidden="true"></i> <span>RSS Reports</span></a>
             </li>
-            <?php } if( is_role('administrator') ||  is_role('franchisee') ||  is_role('coach') ){ ?>
+            <?php } if( is_role('administrator') ){ ?>
             <li>
                 <a href="#cities"><i class="fa fa-archive" aria-hidden="true"></i> <span>Cities</span></a>
             </li>
