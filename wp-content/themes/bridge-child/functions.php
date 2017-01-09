528bbb5e7f9b3875a339ab66cad9926284287e27
JLB <web@jlbworks.com>
2017-01-09 09:55:17 -0600

    increase max upload limit

--- wp-content/themes/bridge-child/functions.php
+++ wp-content/themes/bridge-child/functions.php
@@ -355,3 +355,7 @@ function am2_display_includes() {
 }
 add_action( 'wp_footer', 'am2_display_includes' );
 ?>
+
+@ini_set( ‘upload_max_size’ , ’64M’ );
+@ini_set( ‘post_max_size’, ’64M’);
+@ini_set( ‘max_execution_time’, ‘300’ );
