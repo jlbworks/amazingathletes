3c589159f1b508b53506bbf8ff001afa3dea9d23
JLB <web@jlbworks.com>
2017-01-09 10:02:44 -0600

    Update functions.php

--- wp-content/themes/bridge-child/functions.php
+++ wp-content/themes/bridge-child/functions.php
@@ -356,9 +356,4 @@ function am2_display_includes() {
 add_action( 'wp_footer', 'am2_display_includes' );
 ?>
 
-<?php
-@ini_set( ‘upload_max_size’ , ’64M’ );
-@ini_set( ‘post_max_size’, ’64M’);
-@ini_set( ‘max_execution_time’, ‘300’ );
 
-?>
