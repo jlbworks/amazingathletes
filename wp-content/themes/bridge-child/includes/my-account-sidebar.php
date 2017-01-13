c8f40cdb1cbece8bec40169a85752a0c3bbe206a
JLB <mitch@jlbworks.com>
2017-01-13 16:50:57 -0600

    another test edit

--- wp-content/themes/bridge-child/includes/my-account-sidebar.php
+++ wp-content/themes/bridge-child/includes/my-account-sidebar.php
@@ -107,8 +107,8 @@ $user_id = "user_".$current_user->ID;
  <input type="hidden" name="directphone" value="<?php the_field('telephone', $user_id); ?>"><!-- required -->
  <input type="hidden" name="officename" value="<?php the_field('franchise_name', $user_id); ?>"><!-- required -->
  <input type="hidden" name="officeaddress1" value="<?php the_field('mailing_address', $user_id); ?>"> <!-- required -->
- <input type="hidden" name="officecity" value="<?php the_field('cc_city', $user_id); ?>"><!-- required -->
- <input type="hidden" name="officestate" value="<?php the_field('cc_state', $user_id); ?>"> <!-- required -->
+ <input type="hidden" name="officecity" value="<?php the_field('city_state_city', $user_id); ?>"><!-- required -->
+ <input type="hidden" name="officestate" value="<?php the_field('city_state_state', $user_id); ?>"> <!-- required -->
  <input type="hidden" name="officezip" value="<?php the_field('zip_code', $user_id); ?>"><!-- required -->
  <input class="cps-button" type=submit value="CUSTOM PROSHOP"> 
 
