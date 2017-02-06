d1f6d7a50f102e44df7dcf9fa0b0e334492f0066
JLB <web@jlbworks.com>
2017-02-06 17:49:19 -0600

    new js

---
+++ wp-content/themes/bridge-child/js/custom_scripts.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function( $ ) {
+
+(function($) { 
+		var scroll_start = 0;
+		var startchange = $('#fo-float-row');
+		var offset = startchange.offset();
+		var request_div = $("#fo-float-row");
+		if (startchange.length){
+			$(document).scroll(function() { 
+				scroll_start = $(this).scrollTop();
+				if(scroll_start > offset.top + (-75)) {
+						request_div.addClass("fo-floated-row")
+				}
+				else {
+					request_div.removeClass("fo-floated-row")
+				}
+			});
+		}
+ })(jQuery);
+
+
+});//close all jquery
