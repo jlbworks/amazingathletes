ef968b92a22341ff7d9017a93dec6becb019a074
JLB <web@jlbworks.com>
2017-02-09 13:48:27 -0600

    update to script

--- wp-content/themes/bridge-child/js/custom_scripts.js
+++ wp-content/themes/bridge-child/js/custom_scripts.js
@@ -21,18 +21,9 @@ jQuery(document).ready(function( $ ) {
 
 	
 	
-function toggle() {
-	var ele = document.getElementById("toggleText");
-	var text = document.getElementById("displayText");
-	if(ele.style.display == "block") {
-    		ele.style.display = "none";
-		text.innerHTML = "show";
-  	}
-	else {
-		ele.style.display = "block";
-		text.innerHTML = "hide";
-	}
-} 
+$("#displayText").click(function(){
+     $( "#toggleText" ).toggle("slow");
+});
 	
 
 	
